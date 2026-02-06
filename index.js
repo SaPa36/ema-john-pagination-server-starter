7ec87a7754a97b0017746cc52b14646f4da1eb03
Sapa <nursapa2001@gmail.com>
2026-02-06 12:05:19 +0600

    connect to the mongodb

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+console.log('User:', process.env.DB_USER); // Does this print nursapa2001_db_user?
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // middleware
 app.use(cors());
@@ -12,7 +14,8 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.deftcj8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
