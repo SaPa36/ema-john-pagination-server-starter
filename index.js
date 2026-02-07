0579f9e4dbaa58f7f16077c28578c6b279d3219b
Sapa <nursapa2001@gmail.com>
2026-02-07 10:21:33 +0600

    implement pagination

--- index.js
+++ index.js
@@ -34,7 +34,14 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+
+        const page = parseInt(req.query.page);
+        const size = parseInt(req.query.size);
+        //console.log("Pagination query", req.query)
+        const result = await productCollection.find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
         res.send(result);
     })
 
